db9a0ed645c5d38198b572b4e0b8f46e9b9126ad
kade <zsc0248@gmail.com>
2023-11-15 23:01:10 +0900

    필터 동작 위한 어노테이션 추가

--- src/main/java/com/miracle/companyservice/CompanyServiceApplication.java
+++ src/main/java/com/miracle/companyservice/CompanyServiceApplication.java
@@ -2,8 +2,12 @@ package com.miracle.companyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+@ServletComponentScan
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
