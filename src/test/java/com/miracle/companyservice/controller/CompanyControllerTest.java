56d825f948eefb3bf14f89d902ba88e8790c4361
kade <zsc0248@gmail.com>
2023-11-15 14:00:33 +0900

    test / 회원가입 시, 토큰 값 일치와 불일치 케이스 테스트 코드 작성 완료

--- src/test/java/com/miracle/companyservice/controller/CompanyControllerTest.java
+++ src/test/java/com/miracle/companyservice/controller/CompanyControllerTest.java
@@ -2,6 +2,7 @@ package com.miracle.companyservice.controller;
 
 import com.google.gson.Gson;
 import com.miracle.companyservice.dto.request.CompanySignUpRequestDto;
+import com.miracle.companyservice.dto.response.ErrorApiResponse;
 import com.miracle.companyservice.dto.response.SuccessApiResponse;
 import com.miracle.companyservice.service.CompanyServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -14,7 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,6 +36,8 @@ class CompanyControllerTest {
     @Test
     @DisplayName("기업 회원가입 테스트 - 컨트롤러")
     void signUpCompany() throws Exception {
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
 
         CompanySignUpRequestDto companySignUpRequestDto = CompanySignUpRequestDto.builder()
                 .name("오라클코리아")
@@ -52,6 +58,14 @@ class CompanyControllerTest {
                         .message("회원가입 성공")
                         .build());
 
+        String privateKey = "TkwkdsladkdlrhdnjfrmqdhodlfjgrpaksgdlwnjTdjdy";
+        String sessionId = "sessionId";
+        String result = sessionId + privateKey;
+        int token = result.hashCode();
+
+        given(request.getHeader("sessionId")).willReturn(sessionId);
+        given(request.getIntHeader("miracle")).willReturn(token);
+
         Gson gson = new Gson();
         String content = gson.toJson(companySignUpRequestDto);
 
@@ -66,4 +80,57 @@ class CompanyControllerTest {
                 .andDo(print())
                 .andReturn();
     }
+
+    @Test
+    @DisplayName("기업 회원가입 테스트 실패 - 컨트롤러")
+    void signUpCompanyFail() throws Exception {
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        CompanySignUpRequestDto companySignUpRequestDto = CompanySignUpRequestDto.builder()
+                .name("오라클코리아")
+                .email("austin@oracle.com")
+                .password(1234)
+                .bno("111-13-14444")
+                .ceoName("오스틴 강")
+                .address("서울 서초구 효령로 335")
+                .employeeNum(3000)
+                .sector("소프트웨어 개발업")
+                .photo("/사진/저장/경로.jpg")
+                .introduction("데이터 베이스 소프트웨어 개발 및 공급을 하고 있는 글로벌 기업, 오라클입니다.")
+                .build();
+
+        given(companyService.signUpCompany(companySignUpRequestDto))
+                .willReturn(ErrorApiResponse.builder()
+                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .code("400_1")
+                        .message("토큰 값이 일치하지 않습니다.")
+                        .exception("[UnauthorizedTokenException]: 토큰 값이 일치하지 않습니다.")
+                        .build());
+
+        String privateKey = "TkwkdsladkdlrhdnjfrmqdhodlfjgrpaksgdlwnjTdjdy";
+        String sessionId = "sessionId";
+        String result = sessionId + privateKey;
+        int token = 000000000000;
+
+        given(request.getHeader("sessionId")).willReturn(sessionId);
+        given(request.getIntHeader("miracle")).willReturn(token);
+
+        Gson gson = new Gson();
+        String content = gson.toJson(companySignUpRequestDto);
+
+        mockMvc.perform(
+                        post("/v1/company/signup")
+                                .content(content)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.httpStatus").value("INTERNAL_SERVER_ERROR"))
+                .andExpect(jsonPath("$.code").value("400_1"))
+                .andExpect(jsonPath("$.message").value("토큰 값이 일치하지 않습니다."))
+                .andExpect(jsonPath("$.exception").value("[UnauthorizedTokenException]: 토큰 값이 일치하지 않습니다."))
+                .andExpect(jsonPath("$.data").doesNotExist())
+                .andDo(print())
+                .andReturn();
+    }
+
 }
