fc39bf306aba4f1c4eb0224dc36f0e7a73318f68
kade <zsc0248@gmail.com>
2023-11-17 02:45:38 +0900

    불필요한 커스텀 예외 삭제

--- src/main/java/com/miracle/companyservice/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package com.miracle.companyservice.exception;
-
-public class InternalServerException extends RuntimeException {
-    public InternalServerException(String message) {
-        super(message);
-    }
-}
