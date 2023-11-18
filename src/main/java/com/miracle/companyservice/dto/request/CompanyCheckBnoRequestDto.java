bfa3847e59b3cc4b2213941f4043afe9ed3fac08
kade <zsc0248@gmail.com>
2023-11-18 22:14:24 +0900

    ToString 추가 및, swagger 명세 관련 어노테이션 작성

--- src/main/java/com/miracle/companyservice/dto/request/CompanyCheckBnoRequestDto.java
+++ src/main/java/com/miracle/companyservice/dto/request/CompanyCheckBnoRequestDto.java
@@ -2,17 +2,24 @@ package com.miracle.companyservice.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
+@ToString
 public class CompanyCheckBnoRequestDto {
 
+    @Schema(
+            description = "사업자 번호. 'nnn-nn-nnnnn' 형식으로 요청해야함.",
+            required = true,
+            example = "111-22-33333"
+    )
     @NotBlank(message = "400_5:사업자번호 값이 없습니다.")
-    @Size(min = 1, max = 50, message = "400_5:사업자번호 길이가 너무 짧거나, 깁니다.")
     @Pattern(regexp = "\\d{3}[-]\\d{2}[-]\\d{5}", message = "400_5:사업자번호 형식에 맞지 않습니다.")
     private final String bno;
 
--- src/main/java/com/miracle/companyservice/dto/request/CompanyCheckEmailRequestDto.java
+++ src/main/java/com/miracle/companyservice/dto/request/CompanyCheckEmailRequestDto.java
@@ -2,15 +2,23 @@ package com.miracle.companyservice.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
+@ToString
 public class CompanyCheckEmailRequestDto {
 
+    @Schema(
+            description = "기업 이메일 오류. 이메일 형식을 지켜야함",
+            required = true,
+            example = "youremail@miracle.com"
+    )
     @NotBlank(message = "400_1:이메일 값이 없습니다.")
     @Email(message = "400_1:이메일 형식 오류")
     @Size(min = 1, max = 50, message = "400_1:이메일 길이가 너무 짧거나, 깁니다.")
