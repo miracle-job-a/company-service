54b00ee42292d5b1dacdce15d3063c5dc0de3dfd
kade <zsc0248@gmail.com>
2023-11-23 20:40:15 +0900

    Feat: 공고의 자기소개서 질문 반환 API 명세 어노테이션

---
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiReturnQuestions.java
@@ -0,0 +1,70 @@
+package com.miracle.companyservice.controller.swagger;
+
+import com.miracle.companyservice.dto.response.CommonApiResponse;
+import com.miracle.companyservice.dto.response.ErrorApiResponse;
+import com.miracle.companyservice.dto.response.SuccessApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "자기소개서 질문 조회", description = "공고에 해당하는 자기소개서 질문을 조회합니다.",
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "성공",
+                                                value = "{\"httpStatus\": 200, \"message\": \"자기소개서 질문 조회 성공\", \"data\": List<questionResponseDto> }")
+
+                                },
+                                schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "400",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "실패 / 기업 불일치",
+                                                value = "{\"httpStatus\": 400, \"message\": \"companyId가 공고의 companyId 값과 다릅니다.\", \"data\": false }"),
+                                        @ExampleObject(
+                                                name = "실패 / 미존재 공고",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400\", \"message\":존재하지 않는 공고입니다. \".\", \"data\": \"false\" }"),
+                                },
+                                schema = @Schema(implementation = CommonApiResponse.class)
+                        )),
+
+                @ApiResponse(responseCode = "401",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "토큰 인증 실패",
+                                        value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"토큰 값이 일치하지 않습니다.\", \"exception\": \"UnauthorizedTokenException\" }"
+                                ),
+                                schema = @Schema(implementation = ErrorApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "500",
+                        description = "서버 에러",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "서버 에러",
+                                        value = "{\"httpStatus\": 500, \"code\": \"500\", \"message\": \"서버에 문제가 생겼습니다. 다시 시도해주세요.\", \"exception\": \"RuntimeException\" }"
+                                ),
+                                schema = @Schema(implementation = ErrorApiResponse.class)
+                        )),
+        })
+public @interface ApiReturnQuestions {
+}
