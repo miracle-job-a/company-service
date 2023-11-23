caa1d45c8f210fbc411a599de7275b2b910c8cde
kade <zsc0248@gmail.com>
2023-11-23 14:07:05 +0900

    Comment: Java doc 주석 추가

--- src/main/java/com/miracle/companyservice/repository/CompanyRepository.java
+++ src/main/java/com/miracle/companyservice/repository/CompanyRepository.java
@@ -15,17 +15,33 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
     Boolean existsByIdAndEmailAndBno(Long id, String email, String bno);
 
     /**
-     * Exists by email boolean.
+     * @author kade
      * @param email the email
      * @return the boolean
-     * @author Kade-jeon
+     * 이메일로 해당 기업이 존재하는지 확인하는 메서드
      */
     Boolean existsByEmail(String email);
 
+    /**
+     * @author kade
+     * @param email
+     * @param password
+     * 이메일과 비밀번호가 일치하는 기업 정보를 가져오는 메서드
+     */
     Optional<Company> findByEmailAndPassword(String email, int password);
 
+    /**
+     * @author kade
+     * @param bno
+     * 사업자 번호로 기업이 존재하는지 확인하는 메서드
+     */
     Boolean existsByBno(String bno);
 
+    /**
+     * @author kade
+     * @param companyId
+     * 기업아이디를 이용해서 해당 기업의 대표이미지를 가져오는 메서드
+     */
     @Query("SELECT c.photo FROM Company c WHERE c.id = :companyId")
     String findPhotoById(Long companyId);
 
