db151db76b03af8974e351d807786ac66f94cd9c
kade <zsc0248@gmail.com>
2023-11-23 14:01:51 +0900

    Feat: FAQ 등록 관련 스웨거 명세 어노테이션 구현

---
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiAddFaq.java
@@ -0,0 +1,89 @@
+package com.miracle.companyservice.controller.swagger;
+
+import com.miracle.companyservice.dto.response.CommonApiResponse;
+import com.miracle.companyservice.dto.response.ErrorApiResponse;
+import com.miracle.companyservice.dto.response.SuccessApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "FAQ 등록", description = "FAQ의 질문과 답을 등록합니다.",
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "성공",
+                                                value = "{\"httpStatus\": 200, \"message\": \"FAQ 등록 성공\", \"data\": true }")
+
+                                },
+                                schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "400",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "실패 / 미존재 기업",
+                                                value = "{\"httpStatus\": 400, \"message\": \"존재하지 않는 companyId 입니다.\", \"data\": false }"),
+
+                                        @ExampleObject(
+                                                name = "실패 / 10개 초과",
+                                                value = "{\"httpStatus\": 400, \"message\": \"FAQ는 10개를 넘을 수 없습니다.\", \"data\": false }"),
+
+                                        @ExampleObject(
+                                                name = "유효성 / 기업아이디 오류 ",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_11\", \"message\":companyId 값이 0보다 작습니다. \".\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 질문 값이 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_12\", \"message\": \"질문 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 답변 값이 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_13\", \"message\": \"답변 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                },
+                                schema = @Schema(implementation = CommonApiResponse.class)
+                        )),
+
+                @ApiResponse(responseCode = "401",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "토큰 인증 실패",
+                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"토큰 값이 일치하지 않습니다.\", \"exception\": \"UnauthorizedTokenException\" }"),
+                                        @ExampleObject(
+                                                name = "회원 인증 실패",
+                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"회원 인증에 실패하여 정보를 요청할 수 없습니다.\", \"data\": \"false\" }"),
+
+                                        @ExampleObject(
+                                                name = "회원 정보에 빈 값이 있음",
+                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"회원 정보에 빈 값이 있어 인증을 할 수 없습니다.\", \"data\": \"false\" }"),
+                                },
+                        schema = @Schema(implementation = ErrorApiResponse.class)
+                )),
+                @ApiResponse(responseCode = "500",
+                        description = "서버 에러",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "서버 에러",
+                                        value = "{\"httpStatus\": 500, \"code\": \"500\", \"message\": \"서버에 문제가 생겼습니다. 다시 시도해주세요.\", \"exception\": \"RuntimeException\" }"
+                                ),
+                                schema = @Schema(implementation = ErrorApiResponse.class)
+                        )),
+        })
+public @interface ApiAddFaq {
+}
