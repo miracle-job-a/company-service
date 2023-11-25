4bb309a2d1a6d6be9e1638387c07b3a47987666e
kade <zsc0248@gmail.com>
2023-11-25 17:36:47 +0900

    Refactor: 불필요한 companyId 검증 if문 삭제에 따른 swagger 명세 수정

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiGetFaq.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiGetFaq.java
@@ -38,9 +38,6 @@ import java.lang.annotation.Target;
                                         @ExampleObject(
                                                 name = "실패 / 미존재 기업",
                                                 value = "{\"httpStatus\": 400, \"message\": \"존재하지 않는 companyId 입니다.\", \"data\": false }"),
-                                        @ExampleObject(
-                                                name = "실패 / FAQ 아이디 오류 ",
-                                                value = "{\"httpStatus\": 400, \"code\": \"400_11\", \"message\": \"companyId 값이 0보다 작습니다.\", \"data\": \"false\" }"),
                                 },
                                 schema = @Schema(implementation = CommonApiResponse.class)
                         )),
