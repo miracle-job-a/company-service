8fbc4efdce43539cfd7ee9ff5793d6021b5c0506
kade <zsc0248@gmail.com>
2023-11-28 15:46:25 +0900

    Fix: Swagger 통해 공통 에러 리스폰스 처리에 따라 Responses 정리

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiGetEndPosts.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiGetEndPosts.java
@@ -29,47 +29,6 @@ import java.lang.annotation.Target;
                                 },
                                 schema = @Schema(implementation = SuccessApiResponse.class)
                         )),
-
-
-                @ApiResponse(responseCode = "401",
-                        description = "비정상 요청",
-                        content = @Content(
-                                mediaType = "application/json",
-                                examples = {
-                                        @ExampleObject(
-                                                name = "토큰 인증 실패",
-                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"토큰 값이 일치하지 않습니다.\", \"exception\": \"UnauthorizedTokenException\" }"),
-                                        @ExampleObject(
-                                                name = "회원 인증 실패",
-                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"회원 인증에 실패하여 정보를 요청할 수 없습니다.\", \"data\": \"false\" }"),
-                                        @ExampleObject(
-                                                name = "회원 정보에 빈 값이 있음",
-                                                value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"회원 정보에 빈 값이 있어 인증을 할 수 없습니다.\", \"data\": \"false\" }"),
-                                },
-                        schema = @Schema(implementation = SuccessApiResponse.class)
-                )),
-                @ApiResponse(responseCode = "403",
-                        description = "권한 없음",
-                        content = @Content(
-                                mediaType = "application/json",
-                                examples = {
-                                        @ExampleObject(
-                                                name = "접근 권한 없음",
-                                                value = "{\"httpStatus\": 403, \"code\": \"403\", \"message\": \"접근 권한이 없습니다.\", \"data\": \"false\" }"),
-
-                                },
-                                schema = @Schema(implementation = SuccessApiResponse.class)
-                        )),
-                @ApiResponse(responseCode = "500",
-                        description = "서버 에러",
-                        content = @Content(
-                                mediaType = "application/json",
-                                examples = @ExampleObject(
-                                        name = "서버 에러",
-                                        value = "{\"httpStatus\": 500, \"code\": \"500\", \"message\": \"서버에 문제가 생겼습니다. 다시 시도해주세요.\", \"exception\": \"RuntimeException\" }"
-                                ),
-                                schema = @Schema(implementation = ErrorApiResponse.class)
-                        )),
         })
 public @interface ApiGetEndPosts {
 }
