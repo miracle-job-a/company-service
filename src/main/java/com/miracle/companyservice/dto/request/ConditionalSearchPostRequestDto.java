f578ddff58851249cb5130bd951a9065943b792d
kade <zsc0248@gmail.com>
2023-11-30 15:34:23 +0900

    Feat: 공고 조건 검색 요청 Dto 유효성 검사 추가

--- src/main/java/com/miracle/companyservice/dto/request/ConditionalSearchPostRequestDto.java
+++ src/main/java/com/miracle/companyservice/dto/request/ConditionalSearchPostRequestDto.java
@@ -1,9 +1,14 @@
 package com.miracle.companyservice.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.Set;
 
 @Getter
@@ -12,9 +17,24 @@ import java.util.Set;
 public class ConditionalSearchPostRequestDto {
 
     private final Set<Long> stackIdSet;
+
+    @Schema(
+            description = "경력값은 0이상의 양수입니다.",
+            required = true,
+            example = "3"
+    )
+    @PositiveOrZero(message = "400_21:경력값은 0이상의 양수입니다.")
+    @NotBlank(message = "400_21:경력 값이 없습니다.")
     private final Integer career;
     private final Set<Long> jobIdSet;
     private final Set<String> addressSet;
+
+    @Schema(
+            description = "마감된 공고 검색 여부는 null 일 수 없습니다.",
+            required = true,
+            example = "false"
+    )
+    @NotNull(message = "400_25:마감된 공고 검색여부 값이 없습니다.")
     private final Boolean includeEnded;
 
     public ConditionalSearchPostRequestDto(Set<Long> stackIdSet, Integer career, Set<Long> jobIdSet, Set<String> addressSet, boolean includeEnded) {
