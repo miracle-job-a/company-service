5065006aa4b00c1b0138d2db8aee4824a8a4f5d8
kade <zsc0248@gmail.com>
2023-11-30 15:33:50 +0900

    Feat: 공고 조건 검색 Swagger 명세 어노테이션

---
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiConditionalSearch.java
@@ -0,0 +1,51 @@
+package com.miracle.companyservice.controller.swagger;
+
+import com.miracle.companyservice.dto.response.CommonApiResponse;
+import com.miracle.companyservice.dto.response.SuccessApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "공고 조건검색", description = "유저가 조건을 설정하여 검색한 결과값을 반환합니다.",
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "성공",
+                                                value = "{\"httpStatus\": 200, \"message\": \"공고 상세 검색 완료 - n페이지\", \"data\": List<ConditionalSearchPostResponseDto> }")
+
+                                },
+                                schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "400",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "유효성 / 경력 값 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_21\", \"message\": \"경력값은 0이상의 양수입니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 경력 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_21\", \"message\": \"경력값이 없습니다..\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 마감공고 여부 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_25\", \"message\": \"마감된 공고 검색여부 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                },
+                                schema = @Schema(implementation = CommonApiResponse.class)
+                        )),
+        })
+public @interface ApiConditionalSearch {
+}
