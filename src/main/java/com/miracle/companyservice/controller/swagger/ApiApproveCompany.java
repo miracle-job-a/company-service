205e3f3eca2192cb91f262491f7230bb78ad2b3a
kade <zsc0248@gmail.com>
2023-12-01 20:30:10 +0900

    Feat: 기업 회원 승인 swagger 어노테이션

---
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiApproveCompany.java
@@ -0,0 +1,32 @@
+package com.miracle.companyservice.controller.swagger;
+
+import com.miracle.companyservice.dto.response.SuccessApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "기업 회원 승인", description = "기업 회원 가입 승인 처리 API",
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "성공",
+                                                value = "{\"httpStatus\": 200, \"message\": \"기업 회원 가입이 승인되었습니다.\", \"data\": true }")
+                                        },
+                                schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+        })
+public @interface ApiApproveCompany {
+}
