61ba1cc751a9d17049b7791f1fbd1c5df91b09fc
wjdals3936 <3936vldzm@naver.com>
2023-12-01 09:47:37 +0900

    Feat: 기업 통합 검색 repository 반환 타입 인터페이스 생성

---
+++ src/main/java/com/miracle/companyservice/dto/response/CompanyListResponseDto.java
@@ -0,0 +1,9 @@
+package com.miracle.companyservice.dto.response;
+
+public interface CompanyListResponseDto {
+    Long getId();
+    String getName();
+    String getPhoto();
+    String getCeoName();
+    String getAddress();
+}
