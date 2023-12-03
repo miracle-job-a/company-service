22ff792ba66b2f89e96895fe62a72e5dde74483b
kade <zsc0248@gmail.com>
2023-12-03 14:28:30 +0900

    Feat: swagger 에러 명세 추가

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiApproveCompany.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiApproveCompany.java
@@ -1,5 +1,6 @@
 package com.miracle.companyservice.controller.swagger;
 
+import com.miracle.companyservice.dto.response.CommonApiResponse;
 import com.miracle.companyservice.dto.response.SuccessApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,6 +27,17 @@ import java.lang.annotation.Target;
                                                 value = "{\"httpStatus\": 200, \"message\": \"기업 회원 가입이 승인되었습니다.\", \"data\": true }")
                                         },
                                 schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "400",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "실패 / 만료된 사업자번호",
+                                                value = "{\"httpStatus\": 400, \"message\": \"만료된 사업자 번호 입니다.\", \"data\": false }"),
+                                },
+                                schema = @Schema(implementation = CommonApiResponse.class)
                         )),
         })
 public @interface ApiApproveCompany {
