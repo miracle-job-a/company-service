fa14e0c64ee9ea0caa2e1e0f4d49172e2790c024
wjdals3936 <3936vldzm@naver.com>
2023-12-04 10:54:47 +0900

    Feat: 공고 수정 swagger 명세 유효성 검사 예외 추가

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiModifyPost.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiModifyPost.java
@@ -39,7 +39,59 @@ import java.lang.annotation.Target;
                                                 value = "{\"httpStatus\": 400, \"message\": \"공고 수정에 실패하였습니다.\", \"data\": false }"),
                                         @ExampleObject(
                                                 name = "실패 / 자소서 문항 없음",
-                                                value = "{\"httpStatus\": 400, \"message\": \"자소서 문항 수정에 실패하였습니다.\", \"data\": false }")
+                                                value = "{\"httpStatus\": 400, \"message\": \"자소서 문항 수정에 실패하였습니다.\", \"data\": false }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 공고 타입 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_19\", \"message\": \"공고 타입 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 공고 타입 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_19\", \"message\": \"공고 타입은 NORMAL 또는 MZ 중 하나여야 합니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 공고 제목 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_15\", \"message\": \"공고 제목 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 경력 값 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_21\", \"message\": \"경력 값은 0을 포함한 양수를 허용합니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 경력 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_21\", \"message\": \"경력 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 마감일 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_16\", \"message\": \"마감일 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 마감일 값 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_16\", \"message\": \"현재 날짜보다 미래의 날짜를 설정해야 합니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 주요 업무 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_22\", \"message\": \"주요 업무 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 근무 조건 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_23\", \"message\": \"근무 조건 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 개발툴 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_17\", \"message\": \"개발툴 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 채용절차 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_24\", \"message\": \"채용절차 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 근무지 값 없음",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_18\", \"message\": \"근무지 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 직무 ID Set 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_11\", \"message\": \"직무 ID 집합 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 스택 ID Set 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_11\", \"message\": \"스택 ID 집합 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 테스트 시작일 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_20\", \"message\": \"현재 날짜보다 미래의 날짜를 설정해야 합니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+                                        @ExampleObject(
+                                                name = "유효성 / 테스트 마감일 오류",
+                                                value = "{\"httpStatus\": 400, \"code\": \"400_20\", \"message\": \"현재 날짜보다 미래의 날짜를 설정해야 합니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
+
+
+
+
                                 },
                                 schema = @Schema(implementation = CommonApiResponse.class)
                         )),
