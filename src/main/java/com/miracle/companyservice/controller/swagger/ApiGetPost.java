5dcd26b4733c107f4c1475e12a4d9413f8b4d843
wjdals3936 <3936vldzm@naver.com>
2023-12-04 10:13:05 +0900

    Refactor: 공고 조회 swagger 명세 예외 추가

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiGetPost.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiGetPost.java
@@ -38,21 +38,11 @@ import java.lang.annotation.Target;
                                                 name = "실패 / 해당 공고 정보 없음",
                                                 value = "{\"httpStatus\": 400, \"message\": \"해당 공고 정보가 없습니다.\", \"data\": false }"),
                                         @ExampleObject(
+                                                name = "실패 / 기업 불일치",
+                                                value = "{\"httpStatus\": 400, \"message\": \"companyId가 공고의 companyId 값과 다릅니다.\", \"data\": false }"),
+                                        @ExampleObject(
                                                 name = "실패 / 해당 공고의 자소서 문항 없음",
-                                                value = "{\"httpStatus\": 400, \"message\": \"해당 공고의 자기소개서 문항이 존재하지 않습니다.\", \"data\": false }")/*,
-                                        @ExampleObject(
-                                                name = "유효성 / 이메일 값 없음",
-                                                value = "{\"httpStatus\": 400, \"code\": \"400_1\", \"message\": \"이메일 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
-                                        @ExampleObject(
-                                                name = "유효성 / 이메일 형식 오류",
-                                                value = "{\"httpStatus\": 400, \"code\": \"400_1\", \"message\": \"이메일 형식 오류.\", \"exception\": \"MethodArgumentNotValidException\" }"),
-                                        @ExampleObject(
-                                                name = "유효성 / 이메일 길이 오류",
-                                                value = "{\"httpStatus\": 400, \"code\": \"400_1\", \"message\": \"이메일 길이가 너무 짧거나, 깁니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),
-
-                                        @ExampleObject(
-                                                name = "유효성 / 비밀번호 값 없음",
-                                                value = "{\"httpStatus\": 400, \"code\": \"400_4\", \"message\": \"비밀번호 값이 없습니다.\", \"exception\": \"MethodArgumentNotValidException\" }"),*/
+                                                value = "{\"httpStatus\": 400, \"message\": \"해당 공고의 자기소개서 문항이 존재하지 않습니다.\", \"data\": false }")
                                 },
                                 schema = @Schema(implementation = CommonApiResponse.class)
                         )),
