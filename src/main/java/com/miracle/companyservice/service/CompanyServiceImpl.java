df2bdd2438f547d44d569959b99575d925bc595d
wjdals3936 <3936vldzm@naver.com>
2023-12-06 22:11:10 +0900

    Feat: 기업명 반환 API 서비스 메서드 구현

--- src/main/java/com/miracle/companyservice/service/CompanyServiceImpl.java
+++ src/main/java/com/miracle/companyservice/service/CompanyServiceImpl.java
@@ -951,4 +951,35 @@ public class CompanyServiceImpl implements CompanyService {
                 .data(Boolean.TRUE)
                 .build();
     }
+
+    @Override
+    public CommonApiResponse getCompanyName(Long postId) {
+        Optional<Post> post = postRepository.findCompanyIdById(postId);
+        if (post == null) {
+            return SuccessApiResponse.builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST.value())
+                    .message("해당 공고에 대한 기업 정보가 없습니다.")
+                    .data(Boolean.FALSE)
+                    .build();
+        }
+
+        String companyName = companyRepository.findNameById(post.get().getCompanyId());
+        if (companyName == null) {
+            return SuccessApiResponse.builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST.value())
+                    .message("기업명을 조회할 수 없습니다.")
+                    .data(Boolean.FALSE)
+                    .build();
+        }
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("companyId", post.get().getCompanyId());
+        data.put("companyName", companyName);
+
+        return SuccessApiResponse.builder()
+                .httpStatus(HttpStatus.OK.value())
+                .message("기업명 조회 성공")
+                .data(data)
+                .build();
+    }
 }
