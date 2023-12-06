eeaedc306d58dd945c0d7f785ea3c6c4298cc7a8
wjdals3936 <3936vldzm@naver.com>
2023-12-06 22:11:34 +0900

    Feat: 기업명 반환 API 레포지토리 메서드 구현

--- src/main/java/com/miracle/companyservice/repository/PostRepository.java
+++ src/main/java/com/miracle/companyservice/repository/PostRepository.java
@@ -12,16 +12,19 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * The interface Post repository.
+ */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
     /**
+     * Find top 3 by end date order by end date asc list.
+     *
+     * @param pageable the pageable
+     * @return List<Post>  현재 날짜와 비교하여 마감임박 공고를 반환합니다. closed = false (마감여부) deleted = false (삭제여부)
      * @author kade
-     * @param pageable
-     * @return List<Post>
-     * 현재 날짜와 비교하여 마감임박 공고를 반환합니다.
-     * closed = false (마감여부)
-     * deleted = false (삭제여부)
      */
     @Query("SELECT p FROM Post p " +
             "WHERE p.closed = false " +
@@ -30,51 +33,58 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
     List<Post> findTop3ByEndDateOrderByEndDateAsc(Pageable pageable);
 
     /**
+     * Find all by closed false and deleted false order by created at desc list.
+     *
+     * @param pageable the pageable
+     * @return List<Post>  closed = 0 && deleted = 최신 공고 반환
      * @author kade
-     * @param pageable
-     * @return List<Post>
-     * closed = 0 && deleted = 최신 공고 반환
      */
     List<Post> findAllByClosedFalseAndDeletedFalseOrderByCreatedAtDesc(Pageable pageable);
 
     /**
+     * Exists by company id and id boolean.
+     *
+     * @param companyId the company id
+     * @param postId    the post id
+     * @return boolean  companyID와 postId가 일치하는 공고 유무 확인
      * @author kade
-     * @param companyId
-     * @param postId
-     * @return boolean
-     * companyID와 postId가 일치하는 공고 유무 확인
      */
     boolean existsByCompanyIdAndId(Long companyId, Long postId);
 
     /**
+     * Count by company id and deleted false long.
+     *
+     * @param companyId the company id
+     * @return Long  해당 기업의 전체 공고 수 반환하는 메서드
      * @author kade
-     * @param companyId
-     * @return Long
-     * 해당 기업의 전체 공고 수 반환하는 메서드
      */
     Long countByCompanyIdAndDeletedFalse(Long companyId); //전체 공고
 
     /**
+     * Count by company id and closed true and deleted false long.
+     *
+     * @param companyId the company id
+     * @return Long  해당 기업의 마감된 공고 수 반환하는 메서드
      * @author kade
-     * @param companyId
-     * @return Long
-     * 해당 기업의 마감된 공고 수 반환하는 메서드
      */
     Long countByCompanyIdAndClosedTrueAndDeletedFalse(Long companyId); //마감공고수
 
     /**
+     * Count by company id and closed false and deleted false long.
+     *
+     * @param companyId the company id
+     * @return Long  해당 기업의 진행 중인 공고를 반환하는 메서드
      * @author kade
-     * @param companyId
-     * @return Long
-     * 해당 기업의 진행 중인 공고를 반환하는 메서드
      */
     Long countByCompanyIdAndClosedFalseAndDeletedFalse(Long companyId); //진행중공고
 
     /**
+     * Find all by company id order by latest page.
+     *
+     * @param companyId the company id
+     * @param pageable  the pageable
+     * @return List<CompanyManagePostsResponseDto>  기업이 공고관리를 들어갔을 때, 최신순으로 정렬하여 반환하는 메서드 (디폴트)
      * @author kade
-     * @param companyId
-     * @return List<CompanyManagePostsResponseDto>
-     * 기업이 공고관리를 들어갔을 때, 최신순으로 정렬하여 반환하는 메서드 (디폴트)
      */
     @Query("SELECT p " +
             "FROM Post p " +
@@ -83,10 +93,12 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
     Page<Post> findAllByCompanyIdOrderByLatest(Long companyId, Pageable pageable);
 
     /**
+     * Find all by company id order by deadline page.
+     *
+     * @param companyId the company id
+     * @param pageable  the pageable
+     * @return List<CompanyManagePostsResponseDto>  기업이 공고 관리를 들어갔을 때, 마감임박 순으로 정렬하는 메서드
      * @author kade
-     * @param companyId
-     * @return List<CompanyManagePostsResponseDto>
-     * 기업이 공고 관리를 들어갔을 때, 마감임박 순으로 정렬하는 메서드
      */
     @Query("SELECT p " +
             "FROM Post p " +
@@ -95,11 +107,12 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
     Page<Post> findAllByCompanyIdOrderByDeadline(Long companyId, Pageable pageable);
 
     /**
+     * Find all by company id order by close page.
+     *
+     * @param companyId the company id
+     * @param pageable  the pageable
+     * @return List<CompanyManagePostsResponseDto>  기업이 공고 관리를 들어갔을 때, 종료된 공고만 정렬하여 보여주는 메서드 진행중 공고는 보이지 않습니다.
      * @author kade
-     * @param companyId
-     * @return List<CompanyManagePostsResponseDto>
-     * 기업이 공고 관리를 들어갔을 때, 종료된 공고만 정렬하여 보여주는 메서드
-     * 진행중 공고는 보이지 않습니다.
      */
     @Query("SELECT p " +
             "FROM Post p " +
@@ -108,11 +121,12 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
     Page<Post> findAllByCompanyIdOrderByClose(Long companyId, Pageable pageable);
 
     /**
+     * Find all by company id order by open page.
+     *
+     * @param companyId the company id
+     * @param pageable  the pageable
+     * @return List<CompanyManagePostsResponseDto>  기업이 공고 관리를 들어갔을 때, 진행 중 공고만 정렬하여 보여주는 메서드 마감된 공고는 보이지 않습니다.
      * @author kade
-     * @param companyId
-     * @return List<CompanyManagePostsResponseDto>
-     * 기업이 공고 관리를 들어갔을 때, 진행 중 공고만 정렬하여 보여주는 메서드
-     * 마감된 공고는 보이지 않습니다.
      */
     @Query("SELECT p " +
             "FROM Post p " +
@@ -121,11 +135,12 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
     Page<Post> findAllByCompanyIdOrderByOpen(Long companyId, Pageable pageable);
 
     /**
+     * Find by keyword page.
+     *
+     * @param keyword  the keyword
+     * @param pageable the pageable
+     * @return Page<PostListResponseDto>  사용자가 키워드를 검색하면 해당 키워드가 포함된 공고 관련 데이터를 조회 후, 최신순으로 반환하는 메서드
      * @author wjdals3936
-     * @param keyword
-     * @param pageable
-     * @return Page<PostListResponseDto>
-     * 사용자가 키워드를 검색하면 해당 키워드가 포함된 공고 관련 데이터를 조회 후, 최신순으로 반환하는 메서드
      */
     @Query("SELECT p FROM Post p WHERE " +
             "p.title LIKE :keyword OR " +
@@ -147,4 +162,12 @@ public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificat
      */
     @Override
     Page<Post> findAll(Specification<Post> spec, Pageable pageable);
+
+    /**
+     * @author wjdals3936
+     * @param postId the post id
+     * @return the long
+     * 공고아이디를 통해 해당 기업아이디를 리턴합니다.
+     */
+    Optional<Post> findCompanyIdById(Long postId);
 }
