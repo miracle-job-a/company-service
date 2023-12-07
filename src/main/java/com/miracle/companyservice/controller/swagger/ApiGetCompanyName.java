6cd55df734cf7aabfcf1a3490033909acb3259d6
wjdals3936 <3936vldzm@naver.com>
2023-12-08 01:51:43 +0900

    Feat: 요청/응답 형태 반환에 따른 기업명 반환 API Swagger 명세 수정

--- src/main/java/com/miracle/companyservice/controller/swagger/ApiGetCompanyName.java
+++ src/main/java/com/miracle/companyservice/controller/swagger/ApiGetCompanyName.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Operation(summary = "공고 아이디에 따른 해당 기업명 조회", description = "기업명을 조회합니다.",
+@Operation(summary = "공고 아이디 Set에 따른 해당 기업명 조회", description = "기업명을 조회합니다.",
         responses = {
                 @ApiResponse(responseCode = "200",
                         description = "정상 요청",
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
                                 examples = {
                                         @ExampleObject(
                                                 name = "성공",
-                                                value = "{\"httpStatus\": 200, \"message\": \"기업명 조회 성공\", \"data\": Map<String, Object> }")
+                                                value = "{\"httpStatus\": 200, \"message\": \"기업명 조회 성공\", \"data\": List<CompanyNameResponseDto> }")
 
                                 },
                                 schema = @Schema(implementation = SuccessApiResponse.class)
@@ -35,11 +35,14 @@ import java.lang.annotation.Target;
                                 mediaType = "application/json",
                                 examples = {
                                         @ExampleObject(
-                                                name = "실패 / 공고 아이디 불일치",
-                                                value = "{\"httpStatus\": 400, \"message\": \"공고 아이디에 해당 공고가 없습니다.\", \"data\": false }"),
+                                                name = "실패 / 공고아이디 집합 값 없음",
+                                                value = "{\"httpStatus\": 400, \"message\": \"공고아이디 집합에 값이 없습니다.\", \"data\": false }"),
                                         @ExampleObject(
-                                                name = "실패 / 기업 아이디 불일치",
-                                                value = "{\"httpStatus\": 400, \"message\": \"기업 아이디에 해당하는 기업이 없습니다.\", \"data\": false }")
+                                                name = "실패 / 공고 정보 조회 실패",
+                                                value = "{\"httpStatus\": 400, \"message\": \"공고 정보가 없습니다.\", \"data\": false }"),
+                                        @ExampleObject(
+                                                name = "실패 / 기업 정보 조회 실패",
+                                                value = "{\"httpStatus\": 400, \"message\": \"기업 정보가 없습니다.\", \"data\": false }")
                                 },
                                 schema = @Schema(implementation = CommonApiResponse.class)
                         )),
