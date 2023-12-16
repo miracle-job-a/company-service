535d0987c1335b91c437d8ff5fda3c9f8aa8cc18
kade <zsc0248@gmail.com>
2023-12-16 16:45:56 +0900

    Feat: JWT 토큰 인증 에러 추가

--- src/main/java/com/miracle/companyservice/controller/ErrorController.java
+++ src/main/java/com/miracle/companyservice/controller/ErrorController.java
@@ -1,6 +1,7 @@
 package com.miracle.companyservice.controller;
 
 import com.miracle.companyservice.controller.swagger.ApiDefault;
+import com.miracle.companyservice.exception.InvalidTokenException;
 import com.miracle.companyservice.exception.UnauthorizedTokenException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,10 @@ public class ErrorController {
     @GetMapping("/errors/token")
     public void errorToken() {
         throw new UnauthorizedTokenException("토큰 값이 일치하지 않습니다.");
+    }
+
+    @GetMapping("/invalid-token")
+    public void invalidToken() {
+        throw new InvalidTokenException("토큰 인증 실패");
     }
 }
